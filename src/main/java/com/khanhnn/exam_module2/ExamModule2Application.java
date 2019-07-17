2f9cb772584fbef94a6f295642dbbb3d73708f79
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-07-17 20:59:39 +0700

    exam module2

---
+++ src/main/java/com/khanhnn/exam_module2/ExamModule2Application.java
@@ -0,0 +1,13 @@
+package com.khanhnn.exam_module2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamModule2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamModule2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/khanhnn/exam_module2/ExamModule2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khanhnn.exam_module2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamModule2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
